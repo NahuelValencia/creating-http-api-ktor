6755efa67d49c3b774a10b600705a11d7cf1611d
NahuelValencia <nahuel.valencia@eventbrite.com>
2021-01-22 15:45:09 -0300

    adding some comments

--- src/main/kotlin/com/jetbrains/handson/httpapi/Application.kt
+++ src/main/kotlin/com/jetbrains/handson/httpapi/Application.kt
@@ -5,6 +5,7 @@ import io.ktor.application.Application
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
+//Entry point of our application
 fun Application.module() {
 
 }
